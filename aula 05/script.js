79efb299b0c86419c0d7c410c73b75cfe4b351fb
Beatriz Santos <beatrizarasantos@icloud.com>
2025-08-02 11:00:00 -0300

    Add files via upload

---
+++ aula 05/script.js
@@ -0,0 +1,3 @@
+document.getElementById ("P1").innerText ="Ola"
+document.getElementById("P2").innerText="Beatriz"
+document.getElementById("fundo de tela").style.backgroundColor="black" /*altera fundo de tela*/
